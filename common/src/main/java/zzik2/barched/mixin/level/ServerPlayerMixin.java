f08accc28d4429a8d231277d17a0b45fcc3be544
zzik2 <cheese.zzik2@gmail.com>
2026-01-29 08:24:12 +0900

    Update ServerPlayerMixin.java

--- common/src/main/java/zzik2/barched/mixin/level/ServerPlayerMixin.java
+++ common/src/main/java/zzik2/barched/mixin/level/ServerPlayerMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,15 +21,10 @@ import zzik2.barched.bridge.entity.PlayerBridge;
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin extends Player implements PlayerBridge {
 
-    private Vec3 lastKnownClientMovement;
+    @Shadow private Vec3 lastKnownClientMovement;
 
     public ServerPlayerMixin(Level level, BlockPos blockPos, float f, GameProfile gameProfile) {
         super(level, blockPos, f, gameProfile);
-    }
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void barched$init(MinecraftServer minecraftServer, ServerLevel serverLevel, GameProfile gameProfile, ClientInformation clientInformation, CallbackInfo ci) {
-        this.lastKnownClientMovement = Vec3.ZERO;
     }
 
     @Override
