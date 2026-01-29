4541c995005771cad6454aadbfc2dccacb9dea38
zzik2 <cheese.zzik2@gmail.com>
2026-01-29 20:15:16 +0900

    spears won't damage fixed

--- common/src/main/java/net/minecraft/world/item/SpearItem.java
+++ common/src/main/java/net/minecraft/world/item/SpearItem.java
@@ -1,8 +1,29 @@
 package net.minecraft.world.item;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
 public class SpearItem extends TieredItem {
 
     public SpearItem(Tier tier, Properties properties) {
         super(tier, properties);
+    }
+
+    @Override
+    public boolean canAttackBlock(BlockState blockState, Level level, BlockPos blockPos, Player player) {
+        return false;
+    }
+
+    @Override
+    public boolean hurtEnemy(ItemStack itemStack, LivingEntity livingEntity, LivingEntity livingEntity2) {
+        return true;
+    }
+
+    public void postHurtEnemy(ItemStack itemStack, LivingEntity livingEntity, LivingEntity livingEntity2) {
+        itemStack.hurtAndBreak(1, livingEntity2, EquipmentSlot.MAINHAND);
     }
 }
